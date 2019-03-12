8f5a6e4dc55d7e4b77b89baefc9cd3c21caeab0d
Yukkyman <46261728+YF0314@users.noreply.github.com>
2019-03-12 15:37:13 +0900

    Create define.php

---
+++ src/config/define.php
@@ -0,0 +1,6 @@
+<?php
+
+define("DB_Server", "MYSQLSERVER"); // MySQLのサーバー
+define("DB_User", "MYSQLUSER"); // MySQLのユーザー
+define("DB_Password", "MYSQLPASSWORD"); // MySQLのパスワード
+define("DB_Name", "MYSQLDATABASE"); // MySQLのデータベース名
